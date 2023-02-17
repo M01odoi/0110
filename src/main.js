437ce0982517f727aea4ab6b4bdce1c32d618198
m01odoi <alexsveshkarev@gmail.com>
2023-02-17 10:56:36 +0200

    delete unused vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './vue/App.vue'
-import Vuex from 'vuex'
 import '@/scss/app.scss'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuex)
 
 new Vue({
   render: h => h(App),
